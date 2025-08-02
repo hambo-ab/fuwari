97f3167d9e1a634a457eabf26ec6bf6038a5f65f
hambo-ab <0000osama.po@gmail.com>
2025-08-02 21:13:09 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://hambo-ab.github.io/",
+	site: "https://hambo-ab.github.io",
 	base: "/fuwari/",
 	trailingSlash: "always",
 	integrations: [
